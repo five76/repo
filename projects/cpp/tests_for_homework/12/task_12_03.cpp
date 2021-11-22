3e51419e50b3471b88982148092ad631e3f83436
five76 <fiv_e76@mail.ru>
2021-11-22 14:28:12 +0500

    add test_12_03.cpp

--- projects/cpp/tests_for_homework/12/task_12_03.cpp
+++ projects/cpp/tests_for_homework/12/task_12_03.cpp
@@ -5,9 +5,24 @@
 Выход - следующее простое число
 */
 
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+
+using namespace std;
+
  int find_simple_number(int N)
  {
     int result = 0;
-    //...
-    return result;
+    while (true)
+    {
+       N++;
+       if ((N%2!=0) && (N%3!=0) && (N%5!=0) && (N%7!=0)) return N;
+    }
  }
+
+int main()
+{
+   cout<<find_simple_number(457)<<endl;
+   return 0;
+}
